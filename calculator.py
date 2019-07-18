145952234aec6f1c987231d9342a8f7e3f542b5d
EC2 Default User <ec2-user@ip-172-31-32-218.us-east-2.compute.internal>
2019-07-18 17:40:40 +0000

    Multi statement lines functional

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 #Calculator
 
-def calculate():
+def calculate(exp):
     plus = False
     minus = False
     multiply = False
     divide = False
-    exp = input("Write your expression below. Use numbers only\n ")
     numbers = []
+    b = 0
     result= 0
     for i in exp:
         if i.isdigit():
@@ -29,7 +29,18 @@ def calculate():
         result = numbers[0] * numbers[1]
     elif divide:
         result = numbers[0] / numbers[1]
-    return result
+    return str(result)
+    
 
+def multi_calculate():
+    exp = input("Write your expression below. Use numbers only\n ")
+    digits = exp.split()
+    while len(digits)>1:
+        num = calculate(digits[0:3])
+        digits.pop(0)
+        digits.pop(0)
+        digits.pop(0)
+        digits.insert(0, num)
+    return digits[0]
 
-print(calculate())
+print(multi_calculate())
