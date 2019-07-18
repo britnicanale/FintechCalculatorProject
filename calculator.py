1a64ca26bb592b259523da4b228e9d0248aecb55
EC2 Default User <ec2-user@ip-172-31-32-218.us-east-2.compute.internal>
2019-07-18 17:05:37 +0000

    MVP reached, calculator can add

--- calculator.py
+++ calculator.py
@@ -1,1 +1,26 @@
-import random
+#Calculator
+
+def calculate():
+    plus = False
+    exp = input("Write your expression below. Use numbers only\n ")
+    numbers = []
+    result= 0
+    for i in exp:
+        if i.isdigit():
+            numbers.append(int(i))
+        if i.isalpha():
+            return "Sorry, characters must be numerical digits"
+        if i == "+":
+            plus = True
+    if plus:
+        result = numbers[0] + numbers[1]
+    # elif minus:
+    #     result = a - b
+    # elif multiply:
+    #     result = a * b
+    # elif divide:
+    #     result = a / b
+    return result
+
+
+print(calculate())
